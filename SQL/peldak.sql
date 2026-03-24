5e0ad3fc2c7432c727f431abd506be86ea91e978
b3ni15 <ballobeni08@gmail.com>
2026-03-24 09:08:08 +0100

    Add initial SQL script for database creation and language definitions

---
+++ SQL/peldak.sql
@@ -0,0 +1,10 @@
+-- DDL, DML, TCL, DQL, DCL
+
+-- DDL: Data Definition Language (Adatdefiniálő nyelv)
+
+-- DML: Data Manipulation Language (Adatmanipulációs nyelv)
+
+-- DQL: Data Query Language: (Select)
+
+-- Adatbezis készítés:
+create database school_db;
