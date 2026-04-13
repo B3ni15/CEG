533fd18f1663962f7b374b6f35a2ccfddb0d529b
devbeni <ballobeni08@gmail.com>
2026-04-13 11:36:09 +0200

    Add SQL tables for drinks and transactions, including constraints and queries

--- SQL/gyakorlas.sql
+++ SQL/gyakorlas.sql
@@ -39,3 +39,36 @@ select phone from clients where name = 'Fanni Bíró';
 
 -- Hány vásárló lakik 'Békéscsaba'-n?
 select count(*) from clients where client_address like '%Békéscsaba%';
+
+create table if not exists drinks (
+    id smallint unsigned primary key,
+    name varchar(128) unique not null,
+    volume smallint unsigned not null,
+    constraint volumeValid check (volume between 100 and 5000),
+    price mediumint unsigned not null,
+    constraint priceValid check (price between 1 and 1000000),
+    retailer_id tinyint unsigned not null,
+    foreign key (retailer_id) references retailers(id),
+);
+
+-- Számoljuk meg hány ALKOHOLMENTES ital árolunk?
+-- Ugyanez, de pl. a 330ml és a 500ml Lager Beer egynek számít.
+
+-- melyik forgalmazó árulja a sprite italt? 
+select company_name from retailers r join drinks d on r.id = d.retailer_id where d.name = 'Sprite';
+
+-- milyen alkohol mentes italokat árol az alföld shop kft.?
+select drinks.name from drinks join retailers on drinks.retailer_id = retailers.id where retailers.company_name = 'Alföld shop Kft.';
+
+-- transactionsRetailers tábla (id, drinks_id, retailer_id, quantity, transaction_date)
+create table if not exists transactionsRetailers (
+    id int unsigned primary key auto_increment,
+    retailer_id tinyint unsigned not null,
+    drink_id smallint unsigned not null,
+    quantity smallint unsigned not null,
+    transaction_date date not null,
+    constraint quantityValid check (quantity > 0),
+    foreign key (retailer_id) references retailers(id),
+    foreign key (drink_id) references drinks(id)
+);
+
