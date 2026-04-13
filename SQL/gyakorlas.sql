bcae54789f0f7a80d09e61645be561a3653f11c3
devbeni <ballobeni08@gmail.com>
2026-04-13 09:14:52 +0200

    Add initial SQL script for creating the 'gyakorlas' database and retailers table

---
+++ SQL/gyakorlas.sql
@@ -0,0 +1,27 @@
+-- Készítsünk egy adatbázist:
+CREATE DATABASE gyakorlas;
+
+-- Készítsunk táblákat:
+-- drinks
+-- retailers
+-- clients
+-- trainsactionsClients
+-- transactionsRetailers
+-- employees
+
+-- retailers tábla (max 10) (id, company_name, company_tax_num, company_address, email, phone)
+create table if not exists retailers (
+    id tinyint unsigned primary key,
+    company_name varchar(128) unique not null,
+    company_tax_num varchar(12) unique not null,
+    company_address varchar(255) not null,
+    email varchar(255),
+    phone varchar(12)
+);
+
+-- Mi az 'Alföld shop Kft.' telefonszáma?
+select phone from retailers where company_name = 'Alföld shop Kft.';
+
+-- Írjuk ki a lakcímet azon forgalmazóknál akik 'Bt.'-k
+select company_address from retailers where company_name like '%Bt.%';
+
