fbe25e09177f845d7658588e75ce9dd63e234394
devbeni <ballobeni08@gmail.com>
2026-04-13 09:30:04 +0200

    Add SQL queries for client information and statistics

--- SQL/gyakorlas.sql
+++ SQL/gyakorlas.sql
@@ -25,3 +25,17 @@ select phone from retailers where company_name = 'Alföld shop Kft.';
 -- Írjuk ki a lakcímet azon forgalmazóknál akik 'Bt.'-k
 select company_address from retailers where company_name like '%Bt.%';
 
+create table if not exists clients (
+    id smallint unsigned primary key,
+    name varchar(128) not null,
+    tax_num varchar(12) unique not null,
+    client_address varchar(255) not null,
+    email varchar(255),
+    phone varchar(12)
+);
+
+-- Mi 'Fanni Bíró' telefonszáma?
+select phone from clients where name = 'Fanni Bíró';
+
+-- Hány vásárló lakik 'Békéscsaba'-n?
+select count(*) from clients where client_address like '%Békéscsaba%';
